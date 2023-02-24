4dbbbd536b5cea72709fe6b29c89b1bf93e6b3f8
derinhelm <derinhelm@yandex.ru>
2023-02-24 16:03:29 +0300

    Удаление лишних комментариев

--- bool-calc/src/Lib.hs
+++ bool-calc/src/Lib.hs
@@ -1,19 +1,16 @@
 module Lib
-    ( Bool (..)
-    , Expr (..)
+    ( Expr (..)
     , eval
     ) where
 
     
 data Expr a
-  = Lit Bool                 -- ^ Целочисленный литерал.
---  | Var a                   -- ^ Переменная.
-  | And (Expr a) (Expr a)   -- ^ Сложение.
-  | Or (Expr a) (Expr a)   -- ^ Умножение.
-  deriving (Show)--, Functor)
+  = Lit Bool
+  | And (Expr a) (Expr a)
+  | Or (Expr a) (Expr a)
+  deriving (Show)
     
 eval :: Expr Bool -> Bool
 eval (Lit n) = n
---eval (Var n) = n
 eval (And e1 e2) = eval e1 && eval e2
 eval (Or e1 e2) = eval e1 || eval e2
